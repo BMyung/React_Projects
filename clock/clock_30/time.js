7c2c0c999c5979e670d52212d17111038bfc59e5
Bryan Myung <bryanmyung@gmail.com>
2020-02-21 18:47:52 -0800

    lifecycle hooks

---
+++ clock/clock_30/time.js
@@ -0,0 +1,24 @@
+class Clock extends React.Component {
+    render(){
+        const currentTime = new Date(),
+        hours = currentTime.getHours(),
+        minutes = currentTime.getMinutes(),
+        seconds = currentTime.getSeconds(),
+        ampm = hours >= 12 ? 'pm' : 'am';
+
+        return (
+            <div className="clock">
+                {
+                    hours == 0 ? 12 :
+                    (hours > 12) ?
+                    hours - 12 : hours        
+                }:{
+      minutes > 9 ? minutes : `0${minutes}`
+    }:{
+      seconds > 9 ? seconds : `0${seconds}`
+    } {ampm}
+    </div>
+        );
+    }
+} 
+ReactDOM.render(<Clock />, document.querySelector("#app"));
